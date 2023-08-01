ffb3da70fe2797057038d7b5b2366b39ed0c5684
Larsen Weigle <95002219+larsenweigle@users.noreply.github.com>
2023-08-01 14:30:01 -0400

    attempt 1 of fixing linter errors

--- src/model/train.py
+++ src/model/train.py
@@ -37,8 +37,22 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    cols = ['Pregnancies', 
+            'PlasmaGlucose', 
+            'DiastolicBloodPressure', 
+            'TricepsThickness', 
+            'SerumInsulin', 
+            'BMI', 
+            'DiabetesPedigree', 
+            'Age']
+    
+    X = df[cols].values
+    y = df['Diabetic'].values
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y, 
+                                                        test_size=0.30, 
+                                                        random_state=0)
 
     return X_train, X_test, y_train, y_test
 
@@ -63,6 +77,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
